d7df5d65d66fabfa0304cc408a1f8618ce256c59
carhol <carhol@student.chalmers.se>
2019-11-06 15:17:59 +0100

    Added indicators for mean and standard deviation, as well as labels

--- PresentMonAnalysis.py
+++ PresentMonAnalysis.py
@@ -1,4 +1,5 @@
 from bokeh.plotting import figure, output_file, show
+from bokeh.models import Span, Label
 import numpy as np
 from PresentMonResult import PresentMonResult, LoadFromCsv
 
@@ -14,6 +15,9 @@ fps = []
 for frame in y:
         fps.append(1000/frame)
 
+mu = np.mean(fps)
+sigma = np.std(fps)
+
 # Set output file
 output_file("Analysis.html")
 
@@ -27,5 +31,17 @@ hist, edges = np.histogram(fps, density=True, bins=len(y))
 p.quad(top=hist, bottom=0, left=edges[:-1], right=edges[1:],
         fill_color="#34d5eb", line_color="#34d5eb")
 
+# Add markers for mean, stdev
+muLine = Span(location=mu, dimension='height', line_color='red', line_width=1)
+sigmaLinePos = Span(location=(mu+sigma), dimension='height', line_color='green', line_width=1)
+sigmaLineNeg = Span(location=(mu-sigma), dimension='height', line_color='green', line_width=1)
+p.renderers.extend([muLine, sigmaLinePos, sigmaLineNeg])
+
+# Create descriptive label:
+dataDescription = "Mean: " + "{:10.3f}".format(mu) + ",   StdDev: " + "{:10.3f}".format(sigma)
+labels = Label(x = 0, y = 0, x_units='screen', y_units='screen', text=dataDescription, border_line_color='black', render_mode='css', background_fill_color='white')
+p.add_layout(labels)
+
+p.legend.append("Test")
 # Output and show
 show(p)
